4c724c35f456a8a6a70c3ace354297d41488fc35
marcelo-lourenco <mlourenco.info@gmail.com>
2024-11-27 00:36:59 -0300

    refactor(extencion): converts repeated codes to functions

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,38 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-// Função para formatar strings em formato seguro para diretórios
+// Function to format strings in safe format for directories
 function formatarString(str: string): string {
-  return str
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .trim()
-    .replace(/\s+/g, "-");
+  return str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim().replace(/\s+/g, "-");
 }
 
-// Função para garantir que um diretório exista
-function ensureDirectoryExistence(dir: string) {
+// Function to ensure a directory exists
+async function ensureDirectoryExistence(dir: string, isRoot: boolean = false) {
   if (!fs.existsSync(dir)) {
+    // Cria o diretório diretamente se não existir
     fs.mkdirSync(dir, { recursive: true });
+    if (isRoot) {
+      console.log(`Diretório raiz criado: ${dir}`);
+    }
+  } else if (isRoot) {
+    // Para o diretório principal, confirma substituição
+    const overwrite = await vscode.window.showQuickPick(['Sim', 'Não'], {
+      placeHolder: `O diretório principal "${dir}" já existe. Deseja substituí-lo?`
+    });
+
+    if (overwrite === 'Sim') {
+      // Remove e recria o diretório principal
+      fs.rmSync(dir, { recursive: true, force: true });
+      fs.mkdirSync(dir, { recursive: true });
+      console.log(`Diretório principal substituído: ${dir}`);
+    } else {
+      console.log(`Substituição cancelada para o diretório principal: ${dir}`);
+    }
   }
 }
 
-// Função para resolver referências ($ref) no Swagger
+
+// Function to resolve references ($ref) in Swagger
 function resolveSchemaReferences(swaggerData: any, schema: any): any {
   if (!schema || !schema.$ref) {
     return schema;
@@ -35,7 +49,7 @@ function resolveSchemaReferences(swaggerData: any, schema: any): any {
       }
     });
 
-    // Resolve $refs aninhados
+    // Resolve nested $refs
     return resolveSchemaReferences(swaggerData, resolvedSchema);
   } catch (err: any) {
     console.error("Schema not found for $ref", err.message);
@@ -43,7 +57,7 @@ function resolveSchemaReferences(swaggerData: any, schema: any): any {
   }
 }
 
-// Função para construir exemplos com base no schema
+// Function to build examples based on the schema
 function buildExampleFromSchema(schema: any): any {
   const example: any = {};
   for (const propName in schema.properties) {
@@ -53,17 +67,22 @@ function buildExampleFromSchema(schema: any): any {
   return example;
 }
 
-// Função para obter valores padrão com base no tipo
+// Function to get default values based on type
 function getDefaultValueForType(property: any): any {
   switch (property.type) {
     case "integer":
-      return 123;
+      return 12345;
     case "number":
       return 123.45;
     case "boolean":
       return true;
     case "string":
-      return "example string";
+      if (property.format && property.format === 'date') {
+        return new Date().toISOString().split('T')[0];
+      } else if (property.format && property.format === 'date-time') {
+        return new Date().toISOString();
+      }
+      return "example";
     case "array":
       return property.items ? [getDefaultValueForType(property.items)] : [];
     default:
@@ -71,21 +90,23 @@ function getDefaultValueForType(property: any): any {
   }
 }
 
-// Função para gerar o conteúdo do teste para Playwright e Cypress
-export async function generateTestContent(
+
+// Function to generate the test script content
+async function generateTestFileContent(
   baseUrl: string,
   pathName: string,
   endpointPath: string,
   tagName: string,
   methods: Record<string, any>,
   swaggerData: any,
-  testFramework: 'playwright' | 'cypress'
+  testFramework: 'playwright' | 'cypress',
+  fileExtension: string
 ): Promise<string> {
 
   const urlVariables = `
 const baseUrl = '${baseUrl}';
 const pathName = '${pathName}';
-const pathUrl  = '${endpointPath.replace(/{.*?}/g, "example")}';
+const pathUrl = '${endpointPath.replace(/{.*?}/g, "example")}';
 const endPoint = \`\${baseUrl}\${pathName}\${pathUrl}\`;
 `;
 
@@ -121,24 +142,27 @@ const endPoint = \`\${baseUrl}\${pathName}\${pathUrl}\`;
           : undefined;
 
       if (testFramework === 'playwright') {
-        return `test('${methodUpperCase}: Should return success', async ({ request }) => {
-          const response = await request.${methodLowerCase}(\`\${endPoint}\`, {
-            params: { ${parameters} }${requestBodyContent ? `, data: ${requestBodyContent}` : ""}
-          });
-          expect(response.status()).toBe(200);
-          ${responseExample ? `expect(await response.json()).toEqual(${JSON.stringify(responseExample, null, 2)});` : ""}
-        });`;
-      }
 
-      else if (testFramework === 'cypress') {
-        return `it('${methodUpperCase}: Should return success', () => {
-          cy.${methodLowerCase}(\`\${endPoint}\`, {
-            params: { ${parameters} }${requestBodyContent ? `, body: ${requestBodyContent}` : ""}
-          }).then((response) => {
-            expect(response.status).to.eq(200);
-            ${responseExample ? `expect(response.body).to.deep.equal(${JSON.stringify(responseExample, null, 2)});` : ""}
-          });
-        });`;
+        return `
+  test('${methodUpperCase}: Should return success', async ({ request }) => {
+    const response = await request.${methodLowerCase}(\`\${endPoint}\`, {
+      params: { ${parameters} }${requestBodyContent ? `, data: ${requestBodyContent}` : ""}
+    });
+    expect(response.status()).toBe(200);
+    ${responseExample ? `expect(await response.json()).toEqual(${JSON.stringify(responseExample, null, 2)});` : ""}
+  });`;
+
+      } else if (testFramework === 'cypress') {
+
+        return `
+  it('${methodUpperCase}: Should return success', () => {
+    cy.${methodLowerCase}(\`\${endPoint}\`, {
+      params: { ${parameters} }${requestBodyContent ? `, body: ${requestBodyContent}` : ""}
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      ${responseExample ? `expect(response.body).to.deep.equal(${JSON.stringify(responseExample, null, 2)});` : ""}
+    });
+  });`;
       }
 
       return '';
@@ -152,167 +176,120 @@ test.describe('${tagName} ${endpointPath}', () => {
 	${testCases.join("\n")}
 });
 `;
-
-  }
-
-  else if (testFramework === 'cypress') {
+  } else if (testFramework === 'cypress') {
     return `/// <reference types="Cypress" />
 ${urlVariables}
 describe('${tagName} ${endpointPath}', () => {
   ${testCases.join("\n")}
 });
 `;
-
   }
 
-return '';
+  return '';
 }
 
-// Função principal de ativação da extensão
+
+async function processFile(uri: vscode.Uri, data: string, framework: 'playwright' | 'cypress', fileExtension: string, processDataFunc: (data: any) => { baseUrl: string; pathName: string; paths: any; info: any }): Promise<string[]> {
+  const frameworkCapitalized = framework.charAt(0).toUpperCase() + framework.slice(1);
+  vscode.window.showInformationMessage(`Started creating scripts for ${frameworkCapitalized}.`);
+
+  const suffix = {
+    playwright: 'test',
+    cypress: 'cy',
+    default: 'spec'
+  }[framework] || 'spec';
+
+  try {
+    const jsonData = JSON.parse(data);
+    const { baseUrl, pathName, paths, info } = processDataFunc(jsonData);
+    const generatedFiles: string[] = [];
+
+    const jsonDir = path.dirname(uri.fsPath);
+    const rootDir = path.join(jsonDir, formatarString(info.title || info.name));
+    generatedFiles.push(`${rootDir}\n`);
+
+    // Ensure the existence of the main directory with confirmation
+    // TODO
+    ensureDirectoryExistence(rootDir, true);
+
+    for (const [endpointPath, methods] of Object.entries(paths)) {
+      const tagName = (Object.values(methods as Record<string, any>)[0]?.tags?.[0]) || "untagged";
+      const dirPath = path.join(rootDir, formatarString(tagName));
+      const relativePath = `${endpointPath.replace(/^\/|\/$/g, "").replace(/\//g, "_")}.${suffix}.${fileExtension}`;
+      const fileName = path.join(dirPath, relativePath);
+
+      // Secure subdirectories without confirmation
+      ensureDirectoryExistence(dirPath);
+
+      const testContent = await generateTestFileContent(baseUrl, pathName, endpointPath, tagName, methods as any, jsonData, framework, fileExtension);
+
+      await fs.promises.writeFile(fileName, testContent, "utf-8");
+      generatedFiles.push(`\\${formatarString(tagName)}\\${relativePath}`);
+    }
+    return generatedFiles;
+  } catch (error: unknown) {
+    const errorMessage = (error as Error)?.message || "Unknown error";
+    vscode.window.showErrorMessage(`Error processing file: ${errorMessage}`);
+    return [];
+  }
+}
+
+
+// Main function of activating the extension
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "api-test-builder" is now active!');
+  console.log('Congratulations, your extension "API Test Builder" is now active!');
 
-  // Comandos para Swagger
-  const disposableSwaggerPlaywright = vscode.commands.registerCommand("apiTestScript.genSwaggerToPlaywright", async (uri: vscode.Uri) => {
-    const swaggerFilePath = uri.fsPath;
-    try {
-      const data = await fs.promises.readFile(swaggerFilePath, "utf-8");
-      const swaggerData = JSON.parse(data);
-
-      const url = new URL(swaggerData.servers[0].url);
-      const baseUrl = `${url.protocol}//${url.host}`;
-      const pathName = url.pathname;
-
-      const jsonDir = path.dirname(swaggerFilePath);
-      const rootDir = path.join(jsonDir, formatarString(swaggerData.info.title));
-
-      ensureDirectoryExistence(rootDir);
-
-      for (const [endpointPath, methods] of Object.entries(swaggerData.paths)) {
-        const tagName = (Object.values(methods as Record<string, any>)[0]?.tags?.[0]) || "untagged";
-        const dirPath = path.join(rootDir, formatarString(tagName));
-        const fileName = path.join(dirPath, `${endpointPath.replace(/^\/|\/$/g, "").replace(/\//g, "_")}.test.ts`);
-
-        ensureDirectoryExistence(dirPath);
-
-        const testContent = await generateTestContent(baseUrl, pathName, endpointPath, tagName, methods as any, swaggerData, 'playwright');
-
-        console.log(`Generating file in: ${fileName}`);
-        await fs.promises.writeFile(fileName, testContent, "utf-8");
-        vscode.window.showInformationMessage(`Generated file: ${fileName}`);
+  const registerCommand = (command: string, framework: 'playwright' | 'cypress', fileExtension: string, processDataFunc: (data: any) => { baseUrl: string; pathName: string; paths: any; info: any }) => {
+    const frameworkCapitalized = framework.charAt(0).toUpperCase() + framework.slice(1);
+    const disposable = vscode.commands.registerCommand(command, async (uri: vscode.Uri) => {
+      const filePath = uri.fsPath;
+      try {
+        const data = await fs.promises.readFile(filePath, "utf-8");
+        const generatedFiles = await processFile(uri, data, framework, fileExtension, processDataFunc);
+        if (generatedFiles.length > 0) {
+          vscode.window.showInformationMessage(`Finished creating scripts for ${frameworkCapitalized}.\nFiles generated in:\n${generatedFiles.join('\n\n')}`);
+        }
+      } catch (error: unknown) {
+        const errorMessage = (error as Error)?.message || "Unknown error";
+        vscode.window.showErrorMessage(`Error processing file: ${errorMessage}`);
       }
-    } catch (error: unknown) {
-      const errorMessage = (error as Error)?.message || "Unknown error";
-      vscode.window.showErrorMessage(`Error processing Swagger: ${errorMessage}`);
-    }
-  });
+    });
+    context.subscriptions.push(disposable);
+  };
 
-  const disposableSwaggerCypress = vscode.commands.registerCommand("apiTestScript.genSwaggerToCypress", async (uri: vscode.Uri) => {
-    const swaggerFilePath = uri.fsPath;
-    try {
-      const data = await fs.promises.readFile(swaggerFilePath, "utf-8");
-      const swaggerData = JSON.parse(data);
+  registerCommand("apiTestScript.genSwaggerToPlaywright", 'playwright', 'ts', (data: any) => ({
+    baseUrl: new URL(data.servers[0].url).origin,
+    pathName: new URL(data.servers[0].url).pathname,
+    paths: data.paths,
+    info: data.info,
+  }));
 
-      const url = new URL(swaggerData.servers[0].url);
-      const baseUrl = `${url.protocol}//${url.host}`;
-      const pathName = url.pathname;
+  registerCommand("apiTestScript.genSwaggerToCypress", 'cypress', 'js', (data: any) => ({
+    baseUrl: new URL(data.servers[0].url).origin,
+    pathName: new URL(data.servers[0].url).pathname,
+    paths: data.paths,
+    info: data.info,
+  }));
 
-      const jsonDir = path.dirname(swaggerFilePath);
-      const rootDir = path.join(jsonDir, formatarString(swaggerData.info.title));
+  registerCommand("apiTestScript.genPostmanToPlaywright", 'playwright', 'ts', (data: any) => ({
+    baseUrl: data.info.baseUrl,
+    pathName: '',
+    paths: data.item.reduce((acc: any, item: any) => {
+      acc[item.request.url.path.join('/')] = { [item.request.method.toLowerCase()]: item.request };
+      return acc;
+    }, {}),
+    info: data.info,
+  }));
 
-      ensureDirectoryExistence(rootDir);
-
-      for (const [endpointPath, methods] of Object.entries(swaggerData.paths)) {
-        const tagName = (Object.values(methods as Record<string, any>)[0]?.tags?.[0]) || "untagged";
-        const dirPath = path.join(rootDir, formatarString(tagName));
-        const fileName = path.join(dirPath, `${endpointPath.replace(/^\/|\/$/g, "").replace(/\//g, "_")}.cy.js`);
-
-        ensureDirectoryExistence(dirPath);
-
-        const testContent = await generateTestContent(baseUrl, pathName, endpointPath, tagName, methods as any, swaggerData, 'cypress');
-
-        console.log(`Generating file in: ${fileName}`);
-        await fs.promises.writeFile(fileName, testContent, "utf-8");
-        vscode.window.showInformationMessage(`Generated file: ${fileName}`);
-      }
-    } catch (error: unknown) {
-      const errorMessage = (error as Error)?.message || "Unknown error";
-      vscode.window.showErrorMessage(`Error processing Swagger: ${errorMessage}`);
-    }
-  });
-
-  // Comandos para Postman
-  const disposablePostmanPlaywright = vscode.commands.registerCommand("apiTestScript.genPostmanToPlaywright", async (uri: vscode.Uri) => {
-    const postmanFilePath = uri.fsPath;
-    try {
-      const data = await fs.promises.readFile(postmanFilePath, "utf-8");
-      const postmanData = JSON.parse(data);
-
-      const baseUrl = postmanData.info.baseUrl;
-
-      const jsonDir = path.dirname(postmanFilePath);
-      const rootDir = path.join(jsonDir, formatarString(postmanData.info.name));
-
-      ensureDirectoryExistence(rootDir);
-
-      for (const item of postmanData.item) {
-        const endpointPath = item.request.url.path.join('/');
-        const methods = { [item.request.method.toLowerCase()]: item.request };
-
-        const tagName = "untagged";  // Placeholder for simplicity
-        const dirPath = path.join(rootDir, formatarString(tagName));
-        const fileName = path.join(dirPath, `${endpointPath.replace(/^\/|\/$/g, "").replace(/\//g, "_")}.test.ts`);
-
-        ensureDirectoryExistence(dirPath);
-
-        const testContent = await generateTestContent(baseUrl, '', endpointPath, tagName, methods as any, postmanData, 'playwright');
-
-        console.log(`Generating file in: ${fileName}`);
-        await fs.promises.writeFile(fileName, testContent, "utf-8");
-        vscode.window.showInformationMessage(`Generated file: ${fileName}`);
-      }
-    } catch (error: unknown) {
-      const errorMessage = (error as Error)?.message || "Unknown error";
-      vscode.window.showErrorMessage(`Error processing Postman: ${errorMessage}`);
-    }
-  });
-
-  const disposablePostmanCypress = vscode.commands.registerCommand("apiTestScript.genPostmanToCypress", async (uri: vscode.Uri) => {
-    const postmanFilePath = uri.fsPath;
-    try {
-      const data = await fs.promises.readFile(postmanFilePath, "utf-8");
-      const postmanData = JSON.parse(data);
-
-      const baseUrl = postmanData.info.baseUrl;
-
-      const jsonDir = path.dirname(postmanFilePath);
-      const rootDir = path.join(jsonDir, formatarString(postmanData.info.name));
-
-      ensureDirectoryExistence(rootDir);
-
-      for (const item of postmanData.item) {
-        const endpointPath = item.request.url.path.join('/');
-        const methods = { [item.request.method.toLowerCase()]: item.request };
-
-        const tagName = "untagged";  // Placeholder for simplicity
-        const dirPath = path.join(rootDir, formatarString(tagName));
-        const fileName = path.join(dirPath, `${endpointPath.replace(/^\/|\/$/g, "").replace(/\//g, "_")}.cy.js`);
-
-        ensureDirectoryExistence(dirPath);
-
-        const testContent = await generateTestContent(baseUrl, '', endpointPath, tagName, methods as any, postmanData, 'cypress');
-
-        console.log(`Generating file in: ${fileName}`);
-        await fs.promises.writeFile(fileName, testContent, "utf-8");
-        vscode.window.showInformationMessage(`Generated file: ${fileName}`);
-      }
-    } catch (error: unknown) {
-      const errorMessage = (error as Error)?.message || "Unknown error";
-      vscode.window.showErrorMessage(`Error processing Postman: ${errorMessage}`);
-    }
-  });
-
-  context.subscriptions.push(disposableSwaggerPlaywright, disposableSwaggerCypress, disposablePostmanPlaywright, disposablePostmanCypress);
+  registerCommand("apiTestScript.genPostmanToCypress", 'cypress', 'js', (data: any) => ({
+    baseUrl: data.info.baseUrl,
+    pathName: '',
+    paths: data.item.reduce((acc: any, item: any) => {
+      acc[item.request.url.path.join('/')] = { [item.request.method.toLowerCase()]: item.request };
+      return acc;
+    }, {}),
+    info: data.info,
+  }));
 }
 
-export function deactivate() {}
+export function deactivate() { }
